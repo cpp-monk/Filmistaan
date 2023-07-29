86362343d4a40b9f376f2d9ec893986bcf94228d
cpp-monk <mail.mg2704@gmail.com>
2023-07-30 01:49:01 +0530

    first commit

---
+++ src/components/header/Header.js
@@ -0,0 +1,18 @@
+import React from "react"
+import "./Header.css"
+import { Link } from "react-router-dom"
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="headerLeft">
+                <Link to="/"><img className="header__icon" src="https://static.vecteezy.com/system/resources/thumbnails/005/165/527/small/gold-golden-f-alphabet-letter-logo-icon-design-company-template-for-luxury-business-vector.jpg" /></Link>
+                <Link to="/movies/popular" style={{textDecoration: "none"}}><span>Popular</span></Link>
+                <Link to="/movies/top_rated" style={{textDecoration: "none"}}><span>Top Rated</span></Link>
+                <Link to="/movies/upcoming" style={{textDecoration: "none"}}><span>Upcoming</span></Link>
+            </div>
+        </div>
+    )
+}
+
+export default Header
